bbd5216f03baabad7823babd0ca959db7aeee39c
kmc7468 <kmc7468@naver.com>
2017-05-05 17:22:50 +0900

    class Dlink

--- Dlink/Dlink/Dlink.hh
+++ Dlink/Dlink/Dlink.hh
@@ -1,9 +1,22 @@
-#ifndef DLINK_HEADER_DLINK_HH
-#define DLINK_HEADER_DLINK_HH
+#pragma once
 
 namespace Dlink
 {
+	class Dlink
+	{
+	public:
+		Dlink() = delete;
+		Dlink(const Dlink& dlink) = delete;
+		Dlink(Dlink&& dlink) noexcept = delete;
+		~Dlink() = delete;
 
+	public:
+		Dlink& operator=(const Dlink& dlink) = delete;
+		Dlink& operator=(Dlink&& dlink) noexcept = delete;
+		bool operator==(const Dlink& dlink) const noexcept = delete;
+		bool operator!=(const Dlink& dlink) const noexcept = delete;
+
+	public:
+		static void parse_command_line(int argc, char* argv[]) noexcept;
+	};
 }
-
-#endif
