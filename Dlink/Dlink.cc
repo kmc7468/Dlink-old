8a90ce36b4f86b9b9426a401ae17c08afc09cff9
kmc7468 <kmc7468@naver.com>
2017-05-05 17:32:19 +0900

    Command Line Parsing

--- Dlink/Dlink.cc
+++ Dlink/Dlink.cc
@@ -30,7 +30,18 @@ namespace Dlink
 		if (argv[1] == "--version")
 		{
 			std::cout << "Dlink 1.0.0\n"
-						 "(C) 2017. kmc7468, gear, Funniest All rights reserved.";
+				"(C) 2017. kmc7468, gear, Funniest All rights reserved.";
+			return false;
+		}
+		else if (argv[1] == "--help")
+		{
+			std::cout << "Usage: Dlink [options...]\n"
+				"Allowed options:\n\n";
+
+			std::cout << "--help [option]	View all commands, or view detailed\n"
+						 "					descriptions of commands.\n"
+						 "--version			View the version of Dlink.\n"
+						 "";
 		}
 	}
 }
