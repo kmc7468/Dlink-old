25d2baf22307f7c9f12fe68c3ee0749849747c46
kmc7468 <kmc7468@naver.com>
2017-05-05 17:40:34 +0900

    Command line parsing bug fix

--- Dlink/Dlink.cc
+++ Dlink/Dlink.cc
@@ -1,22 +1,17 @@
 #include "Dlink/Dlink.hh"
 
+#include <cstring>
 #include <iostream>
 
 namespace Dlink
 {
 	bool Dlink::parse_command_line(int argc, char* argv[])
 	{
-		if (argc == 1)
-		{
-			std::cout << "Error: No input files.";
-			return false;
-		}
-
 		switch (argc)
 		{
 		case 1:
 		{
-			std::cout << "Error: No input files.";
+			std::cout << "Error: No input files.\n";
 			return false;
 		}
 
@@ -27,20 +22,20 @@ namespace Dlink
 
 	bool Dlink::parse_command_line_2(int argc, char* argv[])
 	{
-		if (argv[1] == "--version")
+		if (std::strcmp(argv[1], "--version") == 0)
 		{
 			std::cout << "Dlink 1.0.0\n"
-				"(C) 2017. kmc7468, gear, Funniest All rights reserved.";
+						 "(C) 2017. kmc7468, gear, Funniest All rights reserved.\n";
 			return false;
 		}
-		else if (argv[1] == "--help")
+		else if (std::strcmp(argv[1], "--help") == 0)
 		{
 			std::cout << "Usage: Dlink [options...]\n"
 				"Allowed options:\n\n";
 
-			std::cout << "--help [option]	View all commands, or view detailed\n"
-						 "					descriptions of commands.\n"
-						 "--version			View the version of Dlink.\n"
+			std::cout << "--help [option]   View all commands, or view detailed\n"
+						 "                  descriptions of commands.\n"
+						 "--version         View the version of Dlink.\n"
 						 "";
 		}
 	}
