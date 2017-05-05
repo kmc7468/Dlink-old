de90d7abede366449d68b057f306e2477fd3841e
Gear <dev_kr@naver.com>
2017-05-06 01:06:17 +0900

    Add ParseStruct.hh, ParseStruct.hh

---
+++ Dlink/Dlink/ParseStruct.hh
@@ -0,0 +1,167 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+
+#include "Token.hh"
+
+//Parent nodes
+namespace Dlink
+{
+	struct Node
+	{
+		const std::size_t line = 0;
+
+		virtual std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+		{
+			return std::string(depth*6, '-') + "<Undefined Tree>";
+		};
+
+		virtual ~Node() {};
+	};
+
+	struct Statement : public Node {};	
+	struct Expression : public Node {};
+
+	struct Type
+	{
+		const std::size_t line = 0;
+
+		virtual std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+		{
+			return std::string(depth*6, '-') + "<Undefined Tree>";
+		};
+
+		virtual ~Type() {};
+	};
+}
+
+//Expression nodes
+namespace Dlink
+{
+	struct Integer32 : public Expression
+	{
+		const int32_t data;
+
+		Integer32(int32_t _data) : data(_data) {}
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+
+	struct Float : public Expression
+	{
+		const float data;
+
+		Float(float _data) : data(_data) {}
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+
+	struct String : public Expression
+	{
+		const std::string data;
+
+		String(std::string _data) : data(_data) {}
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+
+	struct Identifier : public Expression
+	{
+		const Token id;
+
+		Identifier(const Token& _id) : id(_id) {}
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+
+	struct UnaryOP : public Expression
+	{
+		const Token op;
+		std::shared_ptr<Expression> rhs;
+
+		UnaryOP(const Token& _op, std::shared_ptr<Expression> _rhs) : op(_op), rhs(_rhs) {}
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+
+	struct BinaryOP : public Expression
+	{
+		const Token op;
+		std::shared_ptr<Expression> lhs, rhs;
+
+		BinaryOP(std::shared_ptr<Expression> _lhs, const Token& _op, std::shared_ptr<Expression> _rhs) 
+			: lhs(_lhs), op(_op), rhs(_rhs) {}
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+
+	struct FuncCall : public Expression
+	{
+		const Identifier id;
+		std::vector<std::shared_ptr<Expression>> args;
+
+		FuncCall(const Identifier& _id, const std::vector<std::shared_ptr<Expression>>& _args) : id(_id), args(_args) {}
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+}
+
+//Statement nodes
+namespace Dlink
+{
+	struct Block : public Statement
+	{
+		const std::vector<std::shared_ptr<Statement>> statements;
+		
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+
+	struct VariableDeclaration : public Statement
+	{
+		std::shared_ptr<Type> type; 
+		const Identifier id;
+		std::shared_ptr<Expression> expression;
+
+		VariableDeclaration(std::shared_ptr<Type> _type, const Identifier& _id) : type(_type), id(_id) {}
+		VariableDeclaration(std::shared_ptr<Type> _type, const Identifier& _id, std::shared_ptr<Expression> _expression) 
+			: type(_type), id(_id), expression(_expression) {}
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+
+	struct FunctionDeclaration : public Statement
+	{
+		std::shared_ptr<Type> ret_type; 
+		const Identifier id;
+		const std::vector<VariableDeclaration> arg_list;
+		std::shared_ptr<Statement> body;
+
+		FunctionDeclaration(std::shared_ptr<Type> _ret_type, const Identifier& _id, const std::vector<VariableDeclaration>& _arg_list, std::shared_ptr<Statement> _body) 
+			: ret_type(_ret_type), id(_id), arg_list(_arg_list), body(_body) {}	
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+
+	struct Return : public Statement
+	{
+		std::shared_ptr<Expression> ret_expr;
+
+		Return(std::shared_ptr<Expression> _ret_expr) : ret_expr(_ret_expr) {}
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+
+	struct ExpressionStatement : public Statement
+	{
+		std::shared_ptr<Expression> expression;
+
+		ExpressionStatement(std::shared_ptr<Expression> _expression) : expression(_expression) {}
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map) override;
+	};
+}
+
+//Type nodes
+namespace Dlink
+{
+	struct IdentifierType : public Type
+	{
+		const Identifier id;
+
+		IdentifierType(const Identifier& _id) : id(_id) {}
+		
+		std::string tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map);
+	};
+}
---
+++ Dlink/ParseStruct.cc
@@ -0,0 +1,129 @@
+#include "Dlink/ParseStruct.hh"
+
+//Expression nodes
+namespace Dlink
+{
+	std::string Integer32::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		return std::string(depth*6, '-') + "Integer32 : " + std::to_string(data);
+	}
+	
+	std::string Float::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		return std::string(depth*6, '-') + "Float : " + std::to_string(data);
+	}
+	
+	std::string String::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		return std::string(depth*6, '-') + "String : " + data;
+	}
+	
+	std::string Identifier::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		return std::string(depth*6, '-') + "Identifier : " + id.data;
+	}
+	std::string UnaryOP::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		std::string tree = std::string(depth*6, '-') + "UnaryOP(" + tokentype_map[op.type] + ")\n";
+		tree += rhs->tree_gen(depth+1, tokentype_map);
+		return tree;
+	}
+	
+	std::string BinaryOP::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		std::string tree = std::string(depth*6, '-') + "BinaryOP(" + tokentype_map[op.type] + ")\n";
+		tree += lhs->tree_gen(depth+1, tokentype_map) + "\n";
+		tree += rhs->tree_gen(depth+1, tokentype_map);
+		return tree;
+	}
+
+	std::string FuncCall::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		std::string tree = std::string(depth*6, '-') + "FuncCall\n";
+		tree +=	std::string(depth*6, '-') + "Name : " + id.id.data + "\n"; 
+		tree += "Args : \n";
+
+		for(auto&& arg : args)
+		{
+			tree += arg->tree_gen(depth+1, tokentype_map) + "\n";
+		}
+
+		return tree;
+	}	
+}
+
+//Statement nodes
+namespace Dlink
+{
+	std::string Block::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		std::string tree = std::string(depth*6, '-') + "Block Begin\n";
+		depth++;
+
+		for(auto statement : statements)
+		{
+			tree += statement->tree_gen(depth, tokentype_map) + "\n";
+		}
+
+		depth--;
+		tree += std::string(depth*6, '-') + "End Block";
+		return tree;
+	}
+	
+	std::string VariableDeclaration::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		std::string tree = std::string(depth*6, '-') + "Variable Decl Begin\n";
+		tree += std::string(depth*6, '-') + "Type : \n";
+		tree += type->tree_gen(depth+1, tokentype_map);
+		tree += std::string(depth*6, '-') + "Name : " + id.id.data + "\n";
+
+		if(expression)
+		{
+			tree += std::string(depth*6, '-') + "Init expression : \n";
+			tree += expression->tree_gen(depth + 1, tokentype_map) + "\n";
+		}
+
+		tree += std::string(depth*6, '-') + "End Variable Decl";
+		return tree;
+	}
+		
+	std::string FunctionDeclaration::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		std::string tree = std::string(depth*6, '-') + "Function Decl Begin\n";
+		tree += std::string(depth*6, '-') + "Return Type : \n";
+		tree += ret_type->tree_gen(depth + 1, tokentype_map) + "\n";
+		tree += std::string(depth*6, '-') + "Name : " + id.id.data + "\n";
+
+		tree += std::string(depth*6, '-') + "Arguments\n";
+		for(auto arg : arg_list)
+		{
+			tree += arg.tree_gen(depth + 1, tokentype_map) + "\n";
+		}
+
+		tree += std::string(depth*6, '-') + "Body\n";
+		tree += body->tree_gen(depth + 1, tokentype_map) + "\n";
+		tree += std::string(depth*6, '-') + "End Func Decl";
+		return tree;
+	}
+
+	std::string Return::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		std::string tree = std::string(depth*6, '-') + "Return\n";
+		tree += ret_expr->tree_gen(depth+1, tokentype_map);
+		return tree;
+	}
+	
+	std::string ExpressionStatement::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		return expression->tree_gen(depth, tokentype_map);
+	}
+}
+
+//Type nodes
+namespace Dlink
+{
+	std::string IdentifierType::tree_gen(std::size_t depth, std::map<TokenType, std::string> tokentype_map)
+	{
+		return std::string(depth*6, '-') + "IdentifierType(" + id.id.data + ")";
+	}
+}
